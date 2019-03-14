81ce39fbb702d8c4ca74235a6454e1c90597ef94
Cindy Clijsters <cindy.clijsters@gmail.com>
2019-03-14 20:13:32 +0100

    Add the entity for the labels

---
+++ src/Entity/Label.php
@@ -0,0 +1,91 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\LabelRepository")
+ */
+class Label
+{
+    const STATUS_ACTIVE   = 'active';
+    const STATUS_INACTIVE = 'inactive';
+    const STATUS_DELETED  = 'deleted';
+    
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $status;
+
+    /**
+     * Get the id
+     * 
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the name
+     * 
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the name
+     * 
+     * @param string $name
+     * 
+     * @return \self
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Set the status
+     * 
+     * @return string|null
+     */
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set the status
+     * 
+     * @param string $status
+     * 
+     * @return \self
+     */
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
