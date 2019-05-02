d9db58fa4868453f4fb8475cda9be81a17d36a88
Cindy Clijsters <cindy.clijsters@vdabgenk.be>
2019-05-02 10:04:32 +0200

    Fixed problem with max key length

--- src/Migrations/Version20190430102645.php
+++ src/Migrations/Version20190430102645.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190430102645 extends AbstractMigration
+final class Version20190502080044 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,8 +22,14 @@ final class Version20190430102645 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE ext_translations (id INT AUTO_INCREMENT NOT NULL, locale VARCHAR(8) NOT NULL, object_class VARCHAR(255) NOT NULL, field VARCHAR(32) NOT NULL, foreign_key VARCHAR(64) NOT NULL, content LONGTEXT DEFAULT NULL, INDEX translations_lookup_idx (locale, object_class, foreign_key), UNIQUE INDEX lookup_unique_idx (locale, object_class, field, foreign_key), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB ROW_FORMAT = DYNAMIC');
+        $this->addSql('CREATE TABLE ext_translations (id INT AUTO_INCREMENT NOT NULL, locale VARCHAR(8) NOT NULL, object_class VARCHAR(255) NOT NULL, field VARCHAR(32) NOT NULL, foreign_key VARCHAR(64) NOT NULL, content LONGTEXT DEFAULT NULL, INDEX translations_lookup_idx (locale, object_class, foreign_key), UNIQUE INDEX lookup_unique_idx (locale, object_class, field, foreign_key), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB ROW_FORMAT = DYNAMIC');
         $this->addSql('ALTER TABLE artist ADD description LONGTEXT NOT NULL');
+        $this->addSql('DROP INDEX log_class_lookup_idx ON ext_log_entries');
+        $this->addSql('DROP INDEX log_version_lookup_idx ON ext_log_entries');
+        $this->addSql('DROP INDEX log_user_lookup_idx ON ext_log_entries');
+        $this->addSql('CREATE INDEX log_class_lookup_idx ON ext_log_entries (object_class)');
+        $this->addSql('CREATE INDEX log_version_lookup_idx ON ext_log_entries (object_id, object_class, version)');
+        $this->addSql('CREATE INDEX log_user_lookup_idx ON ext_log_entries (username)');
     }
 
     public function down(Schema $schema) : void
@@ -33,5 +39,11 @@ final class Version20190430102645 extends AbstractMigration
 
         $this->addSql('DROP TABLE ext_translations');
         $this->addSql('ALTER TABLE artist DROP description');
+        $this->addSql('DROP INDEX log_class_lookup_idx ON ext_log_entries');
+        $this->addSql('DROP INDEX log_user_lookup_idx ON ext_log_entries');
+        $this->addSql('DROP INDEX log_version_lookup_idx ON ext_log_entries');
+        $this->addSql('CREATE INDEX log_class_lookup_idx ON ext_log_entries (object_class(191))');
+        $this->addSql('CREATE INDEX log_user_lookup_idx ON ext_log_entries (username(191))');
+        $this->addSql('CREATE INDEX log_version_lookup_idx ON ext_log_entries (object_id, object_class(191), version)');
     }
 }
